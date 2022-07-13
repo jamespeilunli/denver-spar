3d81214b07bf279beee08fa1889e167d285e2fd8
James Li <jamespeilunli@gmail.com>
2022-07-13 11:35:47 -0700

    fix comment logic on unfinished vs draw

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,13 +8,19 @@ class TicTacToe
   end
 
   def winner
+    unfinished = false
     # row checks (go through each row and check if it causes a win)
 
     # column checks (go through each column and check if it causes a win)
 
     # diagonal checks (check both diagonals and see if one causes a win)
-    
-    # if no row, column, or diagonal is a winning string, then the game is unfinished and it's a draw
+
+    # if no row, column, or diagonal is a winning string,
+    # and there are spaces on the board, then the game is unfinished
     return "unfinished"
+    
+    # if no row, column, or diagonal is a winning string, 
+    # and there are only o's and x's on the board, then the game is a draw
+    return "draw"
   end
 end
